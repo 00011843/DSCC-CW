ee6146170e1edd668102f4583b60e1700b9acb40
00011843 <wiut00011843@gmail.com>
2023-11-07 04:16:17 +0500

    Add controller and views for MVC project

---
+++ Frontend/Frontend/Controllers/CompaniesController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Frontend.Models;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Frontend.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        Uri baseAddress = new Uri("http://localhost:5208/api");
+        private readonly HttpClient _client;
+
+        public CompaniesController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+        // GET: CompaniesController
+        public ActionResult Index()
+        {
+            List<Company> companiesList = new List<Company>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/companies").Result;
+               
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                companiesList = JsonConvert.DeserializeObject<List<Company>>(data);
+            }
+            return View(companiesList);
+        }
+
+        // GET: CompaniesController/Details/5
+        public ActionResult Details(int id)
+        {
+            HttpResponseMessage response = _client.GetAsync($"companies/{id}").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                Company company = JsonConvert.DeserializeObject<Company>(data);
+                return View(company);
+            }
+            else
+            {
+                // Handle errors, e.g., display an error page or message
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // GET: CompaniesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CompaniesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(company), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = _client.PostAsync(_client.BaseAddress + "/companies", content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    // Handle errors, e.g., display error messages
+                    ModelState.AddModelError(string.Empty, "Failed to create the company. Please check your input.");
+                }
+            }
+
+            return View(company);
+        }
+
+        // GET: CompaniesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Company company = new Company();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + $"/companies/{id}").Result;
+            
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                company = JsonConvert.DeserializeObject<Company>(data);
+            }
+
+            return View(company);
+        }
+
+        // POST: CompaniesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(company), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = _client.PutAsync(_client.BaseAddress + $"/companies/{id}", content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    // Handle errors, e.g., display error messages
+                    ModelState.AddModelError(string.Empty, "Failed to update the company. Please check your input.");
+                }
+            }
+
+            return View(company);
+        }
+
+        // GET: CompaniesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Company company = new Company();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/companies/" + id).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                company = JsonConvert.DeserializeObject<Company>(data);
+            }
+            return View(company);
+        }
+
+        // POST: CompaniesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Company company)
+        {
+            try
+            {
+                HttpResponseMessage response = _client.DeleteAsync(_client.BaseAddress + $"/companies/{id}").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception ex) 
+            {
+                // Handle errors, e.g., display an error page or message
+                ModelState.AddModelError(string.Empty, "Failed." + ex.Message);
+                return View();
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontend/Frontend/Controllers/VacanciesController.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Frontend.Models;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using System.Text;
+using System.Globalization;
+
+namespace Frontend.Controllers
+{
+    public class VacanciesController : Controller
+    {
+        Uri baseAddress = new Uri("http://localhost:5208/api");
+        private readonly HttpClient _client;
+
+        public VacanciesController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+        // GET: VacanciesController
+        public ActionResult Index()
+        {
+            List<Vacancy> vacanciesList = new List<Vacancy>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/vacancies").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                vacanciesList = JsonConvert.DeserializeObject<List<Vacancy>>(data);
+            }
+            return View(vacanciesList);
+        }
+
+        // GET: VacanciesController/Details/5
+        public ActionResult Details(int id)
+        {
+            HttpResponseMessage response = _client.GetAsync($"vacancies/{id}").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                Vacancy vacancy = JsonConvert.DeserializeObject<Vacancy>(data);
+                return View(vacancy);
+            }
+            else
+            {
+                // Handle errors, e.g., display an error page or message
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // GET: VacanciesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VacanciesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Vacancy vacancy)
+        {
+            if (ModelState.IsValid)
+            {
+                vacancy.JobOpenedDate = DateTime.ParseExact("2023-11-06T22:11:44.240Z", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                vacancy.JobClosedDate = DateTime.ParseExact("2023-11-06T22:11:44.240Z", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                var content = new StringContent(JsonConvert.SerializeObject(vacancy), Encoding.UTF8, "application/json");
+               
+                HttpResponseMessage response = _client.PostAsync(_client.BaseAddress + "/vacancies", content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    // Handle errors, e.g., display error messages
+                    ModelState.AddModelError(string.Empty, "Failed to create the vacancy. Please check your input.");
+                }
+            }
+
+            return View(vacancy);
+        }
+
+        // GET: VacanciesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Vacancy vacancy = new Vacancy();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + $"/vacancies/{id}").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                vacancy = JsonConvert.DeserializeObject<Vacancy>(data);
+            }
+
+            return View(vacancy);
+        }
+
+        // POST: VacanciesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Vacancy vacancy)
+        {
+            if (ModelState.IsValid)
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(vacancy), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = _client.PutAsync(_client.BaseAddress + $"/vacancies/{id}", content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    // Handle errors, e.g., display error messages
+                    ModelState.AddModelError(string.Empty, "Failed to update the vacancy. Please check your input.");
+                }
+            }
+
+            return View(vacancy);
+        }
+
+        // GET: VacanciesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Vacancy vacancy = new Vacancy();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/vacancies/" + id).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                vacancy = JsonConvert.DeserializeObject<Vacancy>(data);
+            }
+            return View(vacancy);
+        }
+
+        // POST: VacanciesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Vacancy vacancy)
+        {
+            try
+            {
+                HttpResponseMessage response = _client.DeleteAsync(_client.BaseAddress + $"/vacancies/{id}").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle errors, e.g., display an error page or message
+                ModelState.AddModelError(string.Empty, "Failed." + ex.Message);
+                return View();
+            }
+            return View();
+        }
+    }
+}
